dcad1532d6d3b15caf2ba32b062f89eb3abca712
sankitdev <ankitsingh635101@gmail.com>
2025-02-14 13:02:53 +0530

    add base service class for mongoose model operations

---
+++ services/base.service.ts
@@ -0,0 +1,23 @@
+import { Model, FilterQuery, UpdateQuery, Document } from "mongoose";
+
+export class BaseService<T extends Document> {
+  protected model: Model<T>;
+  constructor(model: Model<T>) {
+    this.model = model;
+  }
+  async findAll(filter: FilterQuery<T> = {}) {
+    return this.model.find(filter).exec();
+  }
+  async findOne(filter: FilterQuery<T>) {
+    return this.model.findOne(filter).exec();
+  }
+  async create(docs: Array<T> = []) {
+    return this.model.create(docs);
+  }
+  async update(filter: FilterQuery<T>, update: UpdateQuery<T>) {
+    return this.model.findOneAndUpdate(filter, update, { new: true }).exec();
+  }
+  async delete(filter: FilterQuery<T>) {
+    return this.model.findOneAndDelete(filter).exec();
+  }
+}
