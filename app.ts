ba7e7e4aa781941594c48591f28c016cfb19d88a
sankitdev <ankitsingh635101@gmail.com>
2025-02-15 11:29:06 +0530

    feat: initialize database connection and add logging middleware to the application

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
 import { Hono } from "hono";
-
+import { connectDB } from "./schema";
+import { logger } from "hono/logger";
 const app = new Hono();
-
+app.use(logger());
 app.get("/", (c) => {
   return c.json({ test: "ho rha h", message: "hello" });
 });
+const startServer = async () => {
+  await connectDB();
+  console.log("Server is starting...");
+};
 
-export default app;
+startServer();
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+};
