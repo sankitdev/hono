a8154921fc281aa765fd5af3f324f5fee9387bb1
sankitdev <ankitsingh635101@gmail.com>
2025-02-15 13:22:17 +0530

    feat: add winston logger for improved logging

--- schema/index.ts
+++ schema/index.ts
@@ -3,15 +3,16 @@ import { Session } from "./sessionModel";
 import { Transaction } from "./transactionModel";
 import { User } from "./userModel";
 import config from "../config";
+import Logger from "../utils/winstonLogger";
 
 const connectDB = async () => {
   try {
     const db = await mongoose.connect(config.DBURL);
     if (db) {
-      console.log("Connected to DataBase");
+      Logger.info("Connected to DataBase");
     }
   } catch (error) {
-    console.error("Error connecting Database", error);
+    Logger.error("Error connecting Database", error);
     process.exit();
   }
 };
