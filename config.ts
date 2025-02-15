9424d41c5bf9207fb4f957bd97e9509f3bad8f3a
sankitdev <ankitsingh635101@gmail.com>
2025-02-15 13:21:29 +0530

    feat: enhance configuration with environment variables and logging level

--- config.ts
+++ config.ts
@@ -1,13 +1,19 @@
 interface IConfig {
+  env: string;
   DBURL: string;
+  isProduction: boolean;
   PORT: number | string;
   JWT_SECRET_KEY: string;
+  logLevel: string;
 }
-console.log(process.env.DB_URL);
+const ENV = process.env.BUN_ENV;
 const config: IConfig = {
+  env: ENV as string,
+  isProduction: ENV === "production",
   DBURL: process.env.DB_URL as string,
   PORT: process.env.PORT ?? 5000,
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY as string,
+  logLevel: process.env.LOG_LEVEL || (ENV === "production" ? "info" : "debug"),
 };
 
 export default config;
