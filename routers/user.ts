4d701fdf7685025b20a525f9b7a1170ad1751d56
sankitdev <ankitsingh635101@gmail.com>
2025-02-17 14:01:51 +0530

    feat: add update user route to user router

--- routers/user.ts
+++ routers/user.ts
@@ -1,10 +1,16 @@
 import { Hono } from "hono";
-import { createUser, deleteUser, getUser } from "../controller/user";
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  updateUser,
+} from "../controller/user";
 
 const userRoute = new Hono();
 
 userRoute.get("/", getUser);
 userRoute.post("/", createUser);
+userRoute.patch("/:id", updateUser);
 userRoute.delete("/:id", deleteUser);
 
 export default userRoute;
