d756558584a1bbcc47ca5427f533f0dd1c719ec6
sankitdev <ankitsingh635101@gmail.com>
2025-02-18 16:05:47 +0530

    feat: add CRUD operations for transactions in transaction router

--- routers/transaction.ts
+++ routers/transaction.ts
@@ -1,7 +1,14 @@
 import { Hono } from "hono";
-import { createTransaction } from "../controller/transaction";
+import {
+  createTransaction,
+  deleteTransaction,
+  getTransaction,
+  updateTransaction,
+} from "../controller/transaction";
 
 const transactionRoute = new Hono();
-
+transactionRoute.get("/", getTransaction);
 transactionRoute.post("/", createTransaction);
+transactionRoute.patch("/", updateTransaction);
+transactionRoute.delete("/", deleteTransaction);
 export default transactionRoute;
