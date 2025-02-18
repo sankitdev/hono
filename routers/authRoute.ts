f35d148a273c3ff5877a4aafadeb95e740291d8e
sankitdev <ankitsingh635101@gmail.com>
2025-02-18 15:08:02 +0530

    refactor: remove unnecessary whitespace in authRoute file

--- routers/authRoute.ts
+++ routers/authRoute.ts
@@ -1,8 +1,6 @@
 import { Hono } from "hono";
 import { loginUser, logoutUser } from "../controller/authUser";
-
 const authRoute = new Hono();
-
 authRoute.post("/login", loginUser);
 authRoute.post("/logout", logoutUser);
 export default authRoute;
