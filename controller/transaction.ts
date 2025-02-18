788e7f490ac590290a0ed5758198e3327332272f
sankitdev <ankitsingh635101@gmail.com>
2025-02-18 16:05:13 +0530

    feat: add get, update, and delete transaction functionalities with validation

--- controller/transaction.ts
+++ controller/transaction.ts
@@ -4,10 +4,17 @@ import {
   TransactionModel,
 } from "../schema/transactionModel";
 import { BaseService } from "../services/base.service";
-import { createTransactionSchema } from "../validation/transaction";
+import {
+  createTransactionSchema,
+  updateTransactionSchema,
+} from "../validation/transaction";
 
 const transactionService = new BaseService<ITransaction>(TransactionModel);
-
+const getTransaction = asyncHandler(async (c) => {
+  const trans = await transactionService.findAll({});
+  if (trans.length === 0) return c.json([]);
+  return c.json({ transaction: trans });
+});
 const createTransaction = asyncHandler(async (c) => {
   const body = await c.req.json();
   const parsed = createTransactionSchema.parse(body);
@@ -15,4 +22,21 @@ const createTransaction = asyncHandler(async (c) => {
   return c.json({ trans });
 });
 
-export { createTransaction };
+const updateTransaction = asyncHandler(async (c) => {
+  const id = c.req.param;
+  const body = await c.req.json();
+  const parsed = updateTransactionSchema.parse(body);
+  const updateTrans = await transactionService.update(id, parsed);
+  return c.json({ message: "Updated", updateTrans });
+});
+const deleteTransaction = asyncHandler(async (c) => {
+  const id = c.req.param;
+  await transactionService.delete(id);
+  return c.json({ success: true });
+});
+export {
+  createTransaction,
+  updateTransaction,
+  getTransaction,
+  deleteTransaction,
+};
