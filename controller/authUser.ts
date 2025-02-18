1d59a3b98594143946d689cb7bd178e268dda381
sankitdev <ankitsingh635101@gmail.com>
2025-02-18 16:26:39 +0530

    fix: remove unnecessary non-null assertion in password verification

--- controller/authUser.ts
+++ controller/authUser.ts
@@ -9,7 +9,7 @@ const loginUser = asyncHandler(async (c) => {
   const { email, password } = await c.req.json();
   const user = await userService.findOne({ email });
   if (!user) return c.json({ message: "User not found" }, 404);
-  const passCheck = await Bun.password.verify(password, user?.password!);
+  const passCheck = await Bun.password.verify(password, user?.password);
   if (!passCheck) return c.json({ message: "Not authorized" }, 401);
   const { sessionId } = await createSession(c, user.id);
   return c.json({ message: "Session Created", sessionId });
