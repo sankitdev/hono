872a8732e289fdac208ac39f7022e8258665a752
sankitdev <ankitsingh635101@gmail.com>
2025-02-19 10:29:54 +0530

    feat: enhance authorization middleware to validate user sessions and handle errors

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,20 @@
-import { asyncHandler } from "../helper/async";
+import { Context, Next } from "hono";
+import { ISession, SessionModel } from "../schema/sessionModel";
+import { BaseService } from "../services/base.service";
+const sessionService = new BaseService<ISession>(SessionModel);
+const authorizedUser = async (c: Context, next: Next) => {
+  try {
+    const sessionId = c.req.header("Cookie")?.split("=")[1];
+    if (!sessionId) return c.json({ message: "Please Login" }, 401);
+    const session = await sessionService.findOne({ sessionId });
+    console.log(session);
+    if (!session) return c.json({ message: "No active session" }, 401);
+    const userId = session.userId.toString();
+    c.set("userId", userId); // attaching to context
+    await next();
+  } catch (error) {
+    c.json({ message: error }, 500);
+  }
+};
 
-const authorizedUser = asyncHandler(async (c) => {
-  const sessionId = c.req.header("Cookie")?.split("=")[1];
-  if (!sessionId) return c.json({ message: "No active session" }, 401);
-});
+export default authorizedUser;
