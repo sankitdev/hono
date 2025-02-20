2a2b225d2d5d39864766472818799256018b196d
sankitdev <ankitsingh635101@gmail.com>
2025-02-20 14:30:55 +0530

    feat: enhance user model with firstName, lastName, role, and verification fields

--- schema/userModel.ts
+++ schema/userModel.ts
@@ -2,28 +2,68 @@ import { Document, Model, Schema, Types, model } from "mongoose";
 
 interface IUser extends Document {
   _id: Types.ObjectId;
-  name: string;
+  firstName: string;
+  lastName: string;
   email: string;
+  avatar?: string;
+  password: string;
+  twoFactor: {
+    enabled: boolean;
+    secret?: string;
+  };
+  failedLoginAttempt: Number;
+  lockUntil?: Date | null;
   status: "active" | "suspended" | "banned";
   suspendedUntil?: Date | null;
-  password: string;
-  avatar?: string;
+  role: "customer" | "admin" | "support";
+  isVerified: boolean;
+  verificationToken?: string;
+  verificationCode?: string;
+  verificationExpires?: Date;
+  deletedAt: Date;
 }
 
 const userSchema = new Schema<IUser>(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    // 🏷️ User Identity
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    avatar: String,
+
+    // 🔒 Security & Authentication
+    password: { type: String, required: true },
+    twoFactor: {
+      enabled: { type: Boolean, default: false },
+      secret: { type: String, select: false },
+    },
+    failedLoginAttempt: { type: Number, default: 0 },
+    lockUntil: { type: Date, default: null },
+
+    // 🏛️ Account Status
     status: {
       type: String,
       enum: ["active", "suspended", "banned"],
       default: "active",
+      index: true,
+    },
+    role: {
+      type: String,
+      enum: ["customer", "admin", "support"],
+      default: "customer",
+      index: true,
     },
     suspendedUntil: { type: Date, default: null },
-    password: { type: String, required: true },
-    avatar: String,
+    isVerified: { type: Boolean, default: false },
+    verificationToken: { type: String, select: false },
+    verificationCode: { type: String, select: false },
+    verificationExpires: { type: Date, select: false },
+
+    // 🗑️ Soft Deletion
+    deletedAt: { type: Date, default: null },
   },
   { strict: "throw", timestamps: true }
 );
+
 const UserModel: Model<IUser> = model<IUser>("User", userSchema);
 export { IUser, UserModel };
