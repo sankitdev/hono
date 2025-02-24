ec2736ead1ed516766a25681a65176bb71e9c258
sankitdev <ankitsingh635101@gmail.com>
2025-02-24 15:02:52 +0530

    feat: update verification routes to support link and OTP methods

--- routers/authRoute.ts
+++ routers/authRoute.ts
@@ -1,7 +1,13 @@
 import { Hono } from "hono";
-import { loginUser, logoutUser, verifyLoginUser } from "../controller/authUser";
+import {
+  loginUser,
+  logoutUser,
+  verifyLoginUserWithLink,
+  verifyLoginUserWithOTP,
+} from "../controller/authUser";
 const authRoute = new Hono();
 authRoute.post("/login", loginUser);
 authRoute.post("/logout", logoutUser);
-authRoute.get("/verify", verifyLoginUser);
+authRoute.get("/verify", verifyLoginUserWithLink);
+authRoute.post("/email-verify/:userId", verifyLoginUserWithOTP);
 export default authRoute;
