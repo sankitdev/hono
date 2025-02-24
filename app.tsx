fb331f9309481446d2ef1e67599e3b8662271d6c
Ankit Singh <ankitsingh635101@gmail.com>
2025-02-24 23:43:54 +0530

    feat: replace Top component with welcome message and remove unused root view

--- app.tsx
+++ app.tsx
@@ -1,14 +1,13 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import connectDB from "./db/dbConnect";
-import { Top } from "./views/root";
+
 import router from "./routers";
 const app = new Hono();
 app.use(logger());
 app.route("/", router);
 app.get("/", (c) => {
-  const messages = ["Good Morning", "Good Evening", "Good Night"];
-  return c.html(<Top messages={messages} />);
+  return c.text("Welcome Onboard.. Working on UI");
 });
 app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
 const startServer = async () => {
