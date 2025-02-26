0d68429da3b84cb015b8a413582d53b5654644ed
sankitdev <ankitsingh635101@gmail.com>
2025-02-26 13:37:53 +0530

    feat: add CONFLICT status code to HTTP_STATUS for improved response handling

--- utils/response/responseCodes.ts
+++ utils/response/responseCodes.ts
@@ -1,11 +1,12 @@
 import { ContentfulStatusCode } from "hono/utils/http-status";
 
-export const HTTP_STATUS: Record<string, ContentfulStatusCode> = {
+export const HTTP_STATUS = {
   OK: 200,
   CREATED: 201,
   BAD_REQUEST: 400,
   UNAUTHORIZED: 401,
   FORBIDDEN: 403,
   NOT_FOUND: 404,
+  CONFLICT: 409,
   INTERNAL_SERVER_ERROR: 500,
-};
+} as const satisfies Record<string, ContentfulStatusCode>;
