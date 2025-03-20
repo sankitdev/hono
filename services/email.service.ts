ed1d379da4d5c46447afc36cedb877e4ec7960a6
Ankit Singh <ankitsingh635101@gmail.com>
2025-03-20 23:27:15 +0530

    feat: refactor email service to improve verification token generation and email sending process

--- services/email.service.ts
+++ services/email.service.ts
@@ -2,21 +2,17 @@ import Logger from "../utils/winstonLogger";
 import { Context } from "hono";
 import * as crypto from "node:crypto";
 import * as nodemailer from "nodemailer";
-import config from "../config"
-const {EMAIL_ADDRESS, EMAIL_APP_PASSWORD} = config
+import config from "../config";
+import { IUser } from "../schema/user.model";
+const { EMAIL_ADDRESS, EMAIL_APP_PASSWORD } = config;
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: EMAIL_ADDRESS, 
-    pass: EMAIL_APP_PASSWORD, 
+    user: EMAIL_ADDRESS,
+    pass: EMAIL_APP_PASSWORD,
   },
 });
-const sendEmail = async (
-  c: Context,
-  to: string,
-  token: string,
-  tokenLink: string
-) => {
+const sendEmail = async (to: string, token: string, tokenLink: string) => {
   try {
     const response = await transporter.sendMail({
       from: EMAIL_ADDRESS,
@@ -27,26 +23,37 @@ const sendEmail = async (
           <a href=${tokenLink}>Link</a>
           </div>`,
     });
-    return c.json({ response });
+    return { success: true, response };
   } catch (error) {
     Logger.error("Error:", error);
-    return c.json({ success: false, message: (error as Error).message }, 500);
+    return { sucess: false, message: (error as Error).message };
   }
 };
 
-const generateVerificationToken = async () => {
-  try {
-    const verificationToken = crypto.randomBytes(32).toString("hex");
-    const tokenCode = crypto.randomBytes(3).toString("hex");
-    const verificationCode = parseInt(tokenCode, 16)
-      .toString()
-      .slice(0, 6)
-      .padStart(6, "0");
-    const verificationLink = `http://localhost:3000/auth/verify?token=${verificationToken}`;
-    return { verificationCode, verificationLink, verificationToken };
-  } catch (err) {
-    console.error("crypto support is disabled!");
-    throw new Error("Failed to generate verification token");
+const generateVerificationToken = () => {
+  const verificationToken = crypto.randomBytes(32).toString("hex");
+  const verificationCode = crypto.randomInt(100000, 999999).toString();
+  const verificationLink = `http://localhost:3000/auth/verify?token=${verificationToken}`;
+
+  return { verificationCode, verificationLink, verificationToken };
+};
+
+export const sendVerificationEmail = async (c: Context, user: IUser) => {
+  const { verificationCode, verificationLink, verificationToken } =
+    generateVerificationToken();
+
+  user.verificationCode = verificationCode;
+  user.verificationToken = verificationToken;
+  user.verificationExpires = new Date(Date.now() + 5 * 60 * 1000);
+  await user.save();
+
+  const emailResponse = await sendEmail(
+    user.email,
+    verificationCode,
+    verificationLink
+  );
+  if (!emailResponse.success) {
+    throw new Error("Failed to send verification email");
   }
 };
 
