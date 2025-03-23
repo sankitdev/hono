71f7e09efeb406076e7f53e1bdc8d2e358b8b9dc
Ankit Singh <ankitsingh635101@gmail.com>
2025-03-23 18:53:56 +0530

    feat: update email service to use dynamic email templates for verification emails

--- services/email.service.ts
+++ services/email.service.ts
@@ -1,9 +1,9 @@
 import Logger from "../utils/winstonLogger";
-import { Context } from "hono";
 import * as crypto from "node:crypto";
 import * as nodemailer from "nodemailer";
 import config from "../config";
 import { IUser } from "../schema/user.model";
+import generateEmailHtml from "../views/EmailTemplate";
 const { EMAIL_ADDRESS, EMAIL_APP_PASSWORD } = config;
 const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -12,16 +12,13 @@ const transporter = nodemailer.createTransport({
     pass: EMAIL_APP_PASSWORD,
   },
 });
-const sendEmail = async (to: string, token: string, tokenLink: string) => {
+const sendEmail = async (to: string, subject: string, html: string) => {
   try {
     const response = await transporter.sendMail({
       from: EMAIL_ADDRESS,
       to,
-      subject: "Verify your email",
-      html: `<div>
-          <p>${token}<p>
-          <a href=${tokenLink}>Link</a>
-          </div>`,
+      subject,
+      html,
     });
     return { success: true, response };
   } catch (error) {
@@ -34,23 +31,21 @@ const generateVerificationToken = () => {
   const verificationToken = crypto.randomBytes(32).toString("hex");
   const verificationCode = crypto.randomInt(100000, 999999).toString();
   const verificationLink = `http://localhost:3000/auth/verify?token=${verificationToken}`;
-
   return { verificationCode, verificationLink, verificationToken };
 };
 
-export const sendVerificationEmail = async (c: Context, user: IUser) => {
+export const sendVerificationEmail = async (user: IUser) => {
   const { verificationCode, verificationLink, verificationToken } =
     generateVerificationToken();
-
+  const emailHtml = generateEmailHtml(verificationCode, verificationLink);
   user.verificationCode = verificationCode;
   user.verificationToken = verificationToken;
   user.verificationExpires = new Date(Date.now() + 5 * 60 * 1000);
   await user.save();
-
   const emailResponse = await sendEmail(
     user.email,
-    verificationCode,
-    verificationLink
+    "Verify Your Email",
+    emailHtml
   );
   if (!emailResponse.success) {
     throw new Error("Failed to send verification email");
